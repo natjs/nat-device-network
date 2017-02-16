93b9be11a25362e95f2db057be854529d18cc98f
xuqinchao <18854885007@163.com>
2017-02-16 16:28:05 +0800

    android
    1.修改allow back = false
    2.versionName = 1.0.1

--- android/src/main/java/com/nat/device_network/HLWXNetworkModule.java
+++ android/src/main/java/com/nat/device_network/HLWXNetworkModule.java
@@ -3,7 +3,6 @@ package com.nat.device_network;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.telephony.TelephonyManager;
 import android.util.Log;
 
 import org.json.JSONException;
@@ -55,15 +54,6 @@ public class HLWXNetworkModule{
     public static final String TYPE_4G = "4g";
     public static final String TYPE_NONE = "none";
 
-    //没有网络连接
-    public static final String NETWORN_NONE = "none";
-    //wifi连接
-    public static final String NETWORN_WIFI = "wifi";
-    //手机网络数据连接类型
-    public static final String NETWORN_2G = "2g";
-    public static final String NETWORN_3G = "3g";
-    public static final String NETWORN_4G = "4g";
-    public static final String NETWORN_MOBILE = "unknow";
     private static final String LOG_TAG = "HLWXNetworkModule";
 
     private Context mContext;
@@ -123,7 +113,6 @@ public class HLWXNetworkModule{
         return connectionInfo;
     }
 
-
     private String getType(NetworkInfo info) {
         if (info != null) {
             String type = info.getTypeName().toLowerCase(Locale.US);
@@ -164,73 +153,5 @@ public class HLWXNetworkModule{
             return TYPE_NONE;
         }
         return TYPE_UNKNOWN;
-    }
-
-
-
-    public static String getNetworkState(Context context) {
-        //获取系统的网络服务
-        ConnectivityManager connManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-
-        //如果当前没有网络
-        if (null == connManager)
-            return NETWORN_NONE;
-
-        //获取当前网络类型，如果为空，返回无网络
-        NetworkInfo activeNetInfo = connManager.getActiveNetworkInfo();
-        if (activeNetInfo == null || !activeNetInfo.isAvailable()) {
-            return NETWORN_NONE;
-        }
-
-        // 判断是不是连接的是不是wifi
-        NetworkInfo wifiInfo = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-        if (null != wifiInfo) {
-            NetworkInfo.State state = wifiInfo.getState();
-            if (null != state)
-                if (state == NetworkInfo.State.CONNECTED || state == NetworkInfo.State.CONNECTING) {
-                    return NETWORN_WIFI;
-                }
-        }
-
-        // 如果不是wifi，则判断当前连接的是运营商的哪种网络2g、3g、4g等
-        NetworkInfo networkInfo = connManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
-        if (null != networkInfo) {
-            NetworkInfo.State state = networkInfo.getState();
-            String strSubTypeName = networkInfo.getSubtypeName();
-            if (null != state)
-                if (state == NetworkInfo.State.CONNECTED || state == NetworkInfo.State.CONNECTING) {
-                    switch (activeNetInfo.getSubtype()) {
-                        //如果是2g类型
-                        case TelephonyManager.NETWORK_TYPE_GPRS: // 联通2g
-                        case TelephonyManager.NETWORK_TYPE_CDMA: // 电信2g
-                        case TelephonyManager.NETWORK_TYPE_EDGE: // 移动2g
-                        case TelephonyManager.NETWORK_TYPE_1xRTT:
-                        case TelephonyManager.NETWORK_TYPE_IDEN:
-                            return NETWORN_2G;
-                        //如果是3g类型
-                        case TelephonyManager.NETWORK_TYPE_EVDO_A: // 电信3g
-                        case TelephonyManager.NETWORK_TYPE_UMTS:
-                        case TelephonyManager.NETWORK_TYPE_EVDO_0:
-                        case TelephonyManager.NETWORK_TYPE_HSDPA:
-                        case TelephonyManager.NETWORK_TYPE_HSUPA:
-                        case TelephonyManager.NETWORK_TYPE_HSPA:
-                        case TelephonyManager.NETWORK_TYPE_EVDO_B:
-                        case TelephonyManager.NETWORK_TYPE_EHRPD:
-                        case TelephonyManager.NETWORK_TYPE_HSPAP:
-                            return NETWORN_3G;
-                        //如果是4g类型
-                        case TelephonyManager.NETWORK_TYPE_LTE:
-                            return NETWORN_4G;
-                        default:
-                            //中国移动 联通 电信 三种3G制式
-                            if (strSubTypeName.equalsIgnoreCase("TD-SCDMA") || strSubTypeName.equalsIgnoreCase("WCDMA") || strSubTypeName.equalsIgnoreCase("CDMA2000")) {
-                                return NETWORN_3G;
-                            } else {
-                                return NETWORN_MOBILE;
-                            }
-                    }
-                }
-        }
-        return NETWORN_NONE;
     }
 }
